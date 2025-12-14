ad92eb478130b7e91c903d2d12daee4426ef3880
Lawrence Muia Matheka <mathekalawrence96@gmail.com>
2025-12-14 08:16:54 +0300

    JS functionalities added

---
+++ register.js
@@ -0,0 +1,20 @@
+
+document.getElementById('formCredentials').addEventListener('submit', function(event){
+//Prevents default form submission(reload)
+    event.preventDefault();
+
+    //Getting form values
+    const formData = {
+        firstName: document.getElementById('fname').value,
+        secondName: document.getElementById('sname').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+    };
+
+    //logging to the console
+    console.log('Credentials submitted successfully..');
+    console.log('First Name:', formData.firstName);
+    console.log("Second Name: ", formData.secondName);
+    console.log('Email Address: ', formData.email);
+
+})
